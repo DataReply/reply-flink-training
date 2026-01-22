0ab99cd9929a833d42c60e9182d270b8e86ddc3a
Llazi Gusho <l.gusho@reply.de>
2026-01-22 16:14:51 +0100

    translated exercise 4 to English

--- src/main/java/com/reply/flink/training/exercises/Exercise4_Kafka.java
+++ src/main/java/com/reply/flink/training/exercises/Exercise4_Kafka.java
@@ -3,28 +3,28 @@ package com.reply.flink.training.exercises;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * Esercizio 4: Kafka Source & Sink
- * Obiettivo: Leggere dal topic Kafka 'input', elaborare, scrivere su 'output'.
+ * Exercise 4: Kafka Source & Sink
+ * Goal: Read from Kafka topic 'input', process, write to 'output'.
  * <p>
- * TODO: Configurare Kafka Source (bootstrap servers, group id).
- * TODO: Creare DataStream da Kafka.
- * TODO: Scrivere su Console o su un altro topic Kafka.
+ * TODO: Configure Kafka Source (bootstrap servers, group id).
+ * TODO: Create DataStream from Kafka.
+ * TODO: Write to Console or another Kafka topic.
  */
 public class Exercise4_Kafka {
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // Usare variabile d'ambiente per i broker Kafka (AWS MSK o localhost)
+        // Use environment variable for Kafka brokers (AWS MSK or localhost)
         String broker = System.getenv().getOrDefault("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092");
-        System.out.println("Utilizzo bootstrap servers Kafka: " + broker);
+        System.out.println("Using Kafka bootstrap servers: " + broker);
 
-        // TODO: Definire le properties per Kafka
+        // TODO: Define properties for Kafka
         // Properties props = new Properties();
         // props.setProperty("bootstrap.servers", "localhost:9092");
-        
-        // TODO: Aggiungere Source
 
-        // TODO: Eseguire
+        // TODO: Add Source
+
+        // TODO: Execute
     }
 }
