6053741604aee87299b37ecd03333409cae66d8c
Llazi Gusho <l.gusho@reply.de>
2026-01-22 16:12:54 +0100

    translated exercise 3 to English

--- src/main/java/com/reply/flink/training/exercises/Exercise3_Enrichment.java
+++ src/main/java/com/reply/flink/training/exercises/Exercise3_Enrichment.java
@@ -3,14 +3,14 @@ package com.reply.flink.training.exercises;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * Esercizio 3: Arricchimento
- * Obiettivo: Arricchire uno stream di Transaction con UserLocation da uno stream
- * statico "lento" di stringhe (simulando un DB).
+ * Exercise 3: Enrichment
+ * Goal: Enrich a Transaction stream with UserLocation from a "slow" static
+ * string stream (simulating DB).
  * <p>
- * TODO 1: Creare lo Stream principale di Transaction.
- * TODO 2: Creare uno Stream di Rule/Enrichment (es., "User1->NY").
- * TODO 3: Connettere gli stream (Broadcast State o connect()).
- * TODO 4: In ProcessFunction, memorizzare la Rule nello state, applicarla alla Transaction.
+ * TODO 1: Create Main Transaction Stream.
+ * TODO 2: Create a Rule/Enrichment Stream (e.g., "User1->NY").
+ * TODO 3: Connect the streams (Broadcast State or connect()).
+ * TODO 4: In ProcessFunction, store Rule in state, apply to Transaction.
  */
 public class Exercise3_Enrichment {
 
