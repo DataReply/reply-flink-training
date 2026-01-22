6cfd896f801a9bbfb17c1bd910e5a03fdf1681a8
Llazi Gusho <l.gusho@reply.de>
2026-01-22 16:04:54 +0100

    translated exercise 2 to English

--- src/main/java/com/reply/flink/training/exercises/Exercise2_DataProcessing.java
+++ src/main/java/com/reply/flink/training/exercises/Exercise2_DataProcessing.java
@@ -2,36 +2,28 @@ package com.reply.flink.training.exercises;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
-import com.reply.flink.training.exercises.Exercise2_DataProcessing.Transaction;
-
-import java.time.Duration;
-import java.util.List;
-
 /**
- * Esercizio 2: Elaborazione Dati
- * Obiettivo: Parsare uno stream di Strings in POJO, Filter, KeyBy e Window.
+ * Exercise 2: Data Processing
+ * Goal: Parse a stream of Strings into POJOs, Filter, KeyBy, and Window.
  * <p>
- * Schema dei dati in arrivo: "UserId,Amount,Timestamp"
+ * Incoming Data schema: "UserId,Amount,Timestamp"
  * <p>
- * TODO 1: Mappare String a un POJO Transaction.
- * TODO 2: Filtrare le Transaction con amount < 0 (Frode/Errore).
+ * TODO 1: Map String to a Transaction POJO.
+ * TODO 2: Filter out Transactions with amount < 0 (Fraud/Error).
  * TODO 3: KeyBy UserId.
- * TODO 4: Window (Tumbling Window di 5 secondi).
- * TODO 5: Sommare gli importi per utente/finestra.
- * TODO 6: Stampare i risultati.
+ * TODO 4: Window (Tumbling Window of 5 seconds).
+ * TODO 5: Sum the amounts per user/window.
+ * TODO 6: Print results.
  */
 public class Exercise2_DataProcessing {
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-         DataStream<String> raw = env.fromData(List.of(
-                 "User1,100,1000", "User2,50,2000", "User1,-10,3000", "User1,20,4000", "User2,50,5000"));
+        // DataStream<String> raw = env.fromData(List.of(
+        //         "User1,100,1000", "User2,50,2000", "User1,-10,3000", "User1,20,4000", "User2,50,5000"));
 
-        // TODO implementare la pipeline
+        // TODO implement pipeline
 
         env.execute("Exercise 2");
     }
